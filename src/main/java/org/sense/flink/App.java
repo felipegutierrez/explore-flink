29f2006ba5ac0a6fb0651983e5a3046056a049ed
felipegutierrez <felipe.gutierrez@tu-berlin.de>
2018-12-03 11:01:17 +0100

    Word Count example showing Query Execution Plan (QEP)

---
+++ src/main/java/org/sense/flink/App.java
@@ -0,0 +1,62 @@
+package org.sense.flink;
+
+import java.util.Scanner;
+
+import org.apache.flink.runtime.client.JobExecutionException;
+import org.sense.flink.examples.stream.WordCountFilterQEP;
+
+/**
+ * 
+ * @author Felipe Oliveira Gutierrez
+ *
+ */
+public class App {
+	public static void main(String[] args) throws Exception {
+
+		try {
+			int app = 0;
+			do {
+				System.out.println("0 - exit");
+				System.out.println("1 - World count with Filter and showing the Query Execution Plan (QEP)");
+				// System.out.println("2 - ");
+				// System.out.println("3 - ");
+				// System.out.println("4 - ");
+				System.out.print("    Please enter which application you want to run: ");
+
+				String msg = (new Scanner(System.in)).nextLine();
+				app = Integer.valueOf(msg);
+				switch (app) {
+				case 0:
+					System.out.println("bis später");
+					break;
+				case 1:
+					System.out.println("App 1 selected");
+					new WordCountFilterQEP();
+					app = 0;
+					break;
+				case 2:
+					System.out.println("App 2 selected");
+
+					app = 0;
+					break;
+				case 3:
+					System.out.println("App 3 selected");
+
+					app = 0;
+					break;
+				case 4:
+					System.out.println("App 4 selected");
+
+					app = 0;
+					break;
+				default:
+					System.out.println("No application selected [" + app + "] ");
+					break;
+				}
+			} while (app != 0);
+		} catch (JobExecutionException ce) {
+			System.err.println(
+					"The application was not able to connect to the Netcat. Please make sure that you have initiate a Netcat on the command line (\'nc -lk 9000\')");
+		}
+	}
+}
---
+++ src/main/java/org/sense/flink/examples/stream/WordCountFilterQEP.java
@@ -0,0 +1,45 @@
+package org.sense.flink.examples.stream;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * On the terminal execute "nc -lk 9000", run this class and type words back on
+ * the terminal
+ * 
+ * @author Felipe Oliveira Gutierrez
+ *
+ */
+public class WordCountFilterQEP {
+	public WordCountFilterQEP() throws Exception {
+
+		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+		DataStream<Tuple2<String, Integer>> dataStream = env.socketTextStream("localhost", 9000)
+				.flatMap(new SplitterFlatMap()).keyBy(0) // select the first value as a key
+				.sum(1) // reduce to sum all values with same key
+				.filter(word -> word.f1 >= 3) // use simple filter
+		;
+
+		String executionPlan = env.getExecutionPlan();
+		System.out.println("ExecutionPlan ........................ ");
+		System.out.println(executionPlan);
+		System.out.println("........................ ");
+
+		dataStream.print();
+
+		env.execute("Window WordCount Execution plan details");
+	}
+
+	public static class SplitterFlatMap implements FlatMapFunction<String, Tuple2<String, Integer>> {
+		@Override
+		public void flatMap(String sentence, Collector<Tuple2<String, Integer>> out) throws Exception {
+			for (String word : sentence.split(" ")) {
+				out.collect(new Tuple2<String, Integer>(word, 1));
+			}
+		}
+	}
+}
