2268029f8dba54374e0a17881fea6aaa1de0d9d4
felipegutierrez <felipe.gutierrez@tu-berlin.de>
2018-12-10 15:43:37 +0100

    implemented reducer to multiply matrix A and B

--- src/main/java/org/sense/flink/examples/batch/MatrixMultiplication.java
+++ src/main/java/org/sense/flink/examples/batch/MatrixMultiplication.java
@@ -1,9 +1,6 @@
 package org.sense.flink.examples.batch;
 
-import java.util.HashSet;
-import java.util.Set;
-
-import org.apache.flink.api.common.functions.CoGroupFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.MapPartitionFunction;
 import org.apache.flink.api.common.functions.ReduceFunction;
 import org.apache.flink.api.common.typeinfo.Types;
@@ -48,6 +45,14 @@ public class MatrixMultiplication {
 	 * 3,2,-2
 	 * 4,1,0
 	 * 4,2,4
+	 * 
+	 * matrix AB should be
+	 * | 1 -9|
+	 * |23  4|
+	 * 1,1,1
+	 * 1,2,-9
+	 * 2,1,23
+	 * 2,2,4
 	 * </code>
 	 * 
 	 * @throws Exception
@@ -60,11 +65,15 @@ public class MatrixMultiplication {
 				.fieldDelimiter(",").types(Integer.class, Integer.class, Integer.class)
 				.map(t -> new Tuple4<String, Integer, Integer, Integer>("A", t.f0, t.f1, t.f2))
 				.returns(Types.TUPLE(Types.STRING, Types.INT, Types.INT, Types.INT));
+		System.out.println("Matrix A");
+		matrixA.print();
 
 		DataSet<Tuple4<String, Integer, Integer, Integer>> matrixB = env.readCsvFile("resources/matrixB.csv")
 				.fieldDelimiter(",").types(Integer.class, Integer.class, Integer.class)
 				.map(t -> new Tuple4<String, Integer, Integer, Integer>("B", t.f0, t.f1, t.f2))
 				.returns(Types.TUPLE(Types.STRING, Types.INT, Types.INT, Types.INT));
+		System.out.println("Matrix B");
+		matrixB.print();
 
 		int columnsMatrixB = 2;
 		int linesMatrixA = 2;
@@ -72,22 +81,40 @@ public class MatrixMultiplication {
 		// create key and values for both matrix
 		DataSet<Tuple2<Tuple3<Integer, Integer, Integer>, Integer>> keyValueMatrixA = matrixA
 				.mapPartition(new MapMatrixToKeysAndValues(columnsMatrixB));
+		// System.out.println("Matrix A");
+		// keyValueMatrixA.print();
+
 		DataSet<Tuple2<Tuple3<Integer, Integer, Integer>, Integer>> keyValueMatrixB = matrixB
 				.mapPartition(new MapMatrixToKeysAndValues(linesMatrixA));
+		// System.out.println("Matrix B");
+		// keyValueMatrixB.print();
 
 		// simple union operation on matrix A and B
 		DataSet<Tuple2<Tuple3<Integer, Integer, Integer>, Integer>> matrixAB = keyValueMatrixA.union(keyValueMatrixB);
+		// System.out.println("Matrix AB");
+		// matrixAB.print();
 
-		matrixAB.print();
+		// multiply two cells of both matrix. Ex: A[1,1] * B[1,1]
+		DataSet<Tuple2<Tuple3<Integer, Integer, Integer>, Integer>> matrixAB_01 = matrixAB.groupBy(0)
+				.reduce(new ProductReducer());
+		// System.out.println("Matrix AB 01");
+		// matrixAB_01.print();
 
-		// keyValueMatrixA.coGroup(keyValueMatrixB).where(0).equals(0).with(new
-		// MyCoGroup());
+		// transform key(i,k,i+j) and value(AB[i,j]) into key(i,j) and value(AB[i,j])
+		DataSet<Tuple2<Tuple2<Integer, Integer>, Integer>> matrixAB_02 = matrixAB_01.map(new SumMapper());
+		// System.out.println("Matrix AB 02");
+		// matrixAB_02.print();
+
+		DataSet<Tuple2<Tuple2<Integer, Integer>, Integer>> productMatrixAB = matrixAB_02.groupBy(0)
+				.reduce(new SumReducer());
+		System.out.println("Matrix AB");
+		productMatrixAB.print();
 
 		// env.execute("MatrixMultiplication");
 
 		// String executionPlan = env.getExecutionPlan();
 		// System.out.println("ExecutionPlan ........................ ");
-		// System.out.println(executionPlan);
+		// System.out.println(env.getExecutionPlan());
 		// System.out.println("........................ ");
 	}
 
@@ -120,8 +147,7 @@ public class MatrixMultiplication {
 	public static class MapMatrixToKeysAndValues implements
 			MapPartitionFunction<Tuple4<String, Integer, Integer, Integer>, Tuple2<Tuple3<Integer, Integer, Integer>, Integer>> {
 
-		private static final long serialVersionUID = 1L;
-
+		private static final long serialVersionUID = 6992353073599144457L;
 		private int count;
 
 		public MapMatrixToKeysAndValues(int count) {
@@ -148,11 +174,11 @@ public class MatrixMultiplication {
 						// value matrix[i,j]
 						value = tuple.f3;
 					} else if (MATRIX_B.equals(tuple.f0)) {
-						// key(i,k,j+k) for i=1...L
+						// key(i,k,i+j) for i=1...L
 						Integer i = c;
 						Integer j = tuple.f1;
 						Integer k = tuple.f2;
-						key = new Tuple3<Integer, Integer, Integer>(i, k, j + k);
+						key = new Tuple3<Integer, Integer, Integer>(i, k, i + j);
 
 						// value matrix[j,k]
 						value = tuple.f3;
@@ -163,24 +189,46 @@ public class MatrixMultiplication {
 		}
 	}
 
-	public static class MyCoGroup implements CoGroupFunction<Tuple2<String, Integer>, Tuple2<String, Double>, Double> {
+	/**
+	 * This function just multiply two cells of both matrix. Ex: A[1,1] * B[1,1]
+	 * 
+	 * @author Felipe Oliveira Gutierrez
+	 *
+	 */
+	public static class ProductReducer implements ReduceFunction<Tuple2<Tuple3<Integer, Integer, Integer>, Integer>> {
+
+		private static final long serialVersionUID = 6166767956669902083L;
+
 		@Override
-		public void coGroup(Iterable<Tuple2<String, Integer>> iVals, Iterable<Tuple2<String, Double>> dVals,
-				Collector<Double> out) {
+		public Tuple2<Tuple3<Integer, Integer, Integer>, Integer> reduce(
+				Tuple2<Tuple3<Integer, Integer, Integer>, Integer> value1,
+				Tuple2<Tuple3<Integer, Integer, Integer>, Integer> value2) throws Exception {
 
-			Set<Integer> ints = new HashSet<Integer>();
+			Integer product = null;
+			product = value1.f1 * value2.f1;
 
-			// add all Integer values in group to set
-			for (Tuple2<String, Integer> val : iVals) {
-				ints.add(val.f1);
-			}
+			return new Tuple2<Tuple3<Integer, Integer, Integer>, Integer>(value1.f0, product);
+		}
+	}
 
-			// multiply each Double value with each unique Integer values of group
-			for (Tuple2<String, Double> val : dVals) {
-				for (Integer i : ints) {
-					out.collect(val.f1 * i);
-				}
-			}
+	/**
+	 * This function is just to map all cells that are going to being sum on both
+	 * matrix.
+	 * 
+	 * @author Felipe Oliveira Gutierrez
+	 *
+	 */
+	public static class SumMapper implements
+			MapFunction<Tuple2<Tuple3<Integer, Integer, Integer>, Integer>, Tuple2<Tuple2<Integer, Integer>, Integer>> {
+
+		private static final long serialVersionUID = -1437482917757334157L;
+
+		@Override
+		public Tuple2<Tuple2<Integer, Integer>, Integer> map(Tuple2<Tuple3<Integer, Integer, Integer>, Integer> value)
+				throws Exception {
+
+			Tuple2<Integer, Integer> key = new Tuple2<Integer, Integer>(value.f0.f0, value.f0.f1);
+			return new Tuple2<Tuple2<Integer, Integer>, Integer>(key, value.f1);
 		}
 	}
 
@@ -198,13 +246,18 @@ public class MatrixMultiplication {
 	 * 
 	 * @author Felipe Oliveira Gutierrez
 	 */
-	public static class ProductReducer implements ReduceFunction<Tuple3<Integer, Integer, Integer>> {
+	public static class SumReducer implements ReduceFunction<Tuple2<Tuple2<Integer, Integer>, Integer>> {
+
+		private static final long serialVersionUID = 7849401047616065465L;
+
 		@Override
-		public Tuple3<Integer, Integer, Integer> reduce(Tuple3<Integer, Integer, Integer> v1,
-				Tuple3<Integer, Integer, Integer> v2) {
-			Integer sum = v1.f1 + v2.f1;
-			return new Tuple3<Integer, Integer, Integer>(v1.f0, v2.f0, sum);
+		public Tuple2<Tuple2<Integer, Integer>, Integer> reduce(Tuple2<Tuple2<Integer, Integer>, Integer> value1,
+				Tuple2<Tuple2<Integer, Integer>, Integer> value2) throws Exception {
+
+			Tuple2<Integer, Integer> key = new Tuple2<Integer, Integer>(value1.f0.f0, value1.f0.f1);
+			Integer value = value1.f1 + value2.f1;
+
+			return new Tuple2<Tuple2<Integer, Integer>, Integer>(key, value);
 		}
 	}
-
 }
