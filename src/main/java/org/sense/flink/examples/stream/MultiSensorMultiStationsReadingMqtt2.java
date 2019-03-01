58af9f21f766e1b75d52cf1bf834d74d07088357
felipegutierrez <felipe.gutierrez@tu-berlin.de>
2019-03-01 17:15:47 +0100

    Computing Count Min Sketch over a window with ProcessingWindowFunction

--- src/main/java/org/sense/flink/examples/stream/MultiSensorMultiStationsReadingMqtt2.java
+++ src/main/java/org/sense/flink/examples/stream/MultiSensorMultiStationsReadingMqtt2.java
@@ -3,10 +3,15 @@ package org.sense.flink.examples.stream;
 import org.apache.flink.api.common.functions.AggregateFunction;
 import org.apache.flink.api.common.functions.FilterFunction;
 import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.api.common.state.ValueState;
+import org.apache.flink.api.common.state.ValueStateDescriptor;
 import org.apache.flink.api.java.functions.KeySelector;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.tuple.Tuple3;
+import org.apache.flink.api.java.tuple.Tuple4;
 import org.apache.flink.api.java.tuple.Tuple5;
+import org.apache.flink.api.java.typeutils.TupleTypeInfo;
+import org.apache.flink.shaded.curator.org.apache.curator.shaded.com.google.common.collect.Iterables;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -23,8 +28,6 @@ public class MultiSensorMultiStationsReadingMqtt2 {
 
 	public MultiSensorMultiStationsReadingMqtt2() throws Exception {
 
-		final CountMinSketch countMinSketch = new CountMinSketch();
-
 		// Start streaming from fake data source sensors
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -38,32 +41,32 @@ public class MultiSensorMultiStationsReadingMqtt2 {
 		// streamStations.print();
 
 		// @formatter:off
-		streamStations.filter(new SensorFilter("COUNT_PE"))
+		streamStations.filter(new SensorFilter(new String[]{"COUNT_PE"}))
 				.map(new TrainStationMapper())
 				.keyBy(0)
 				.window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-				.aggregate(new AverageAggregator("COUNT_PE", countMinSketch))
+				.aggregate(new AverageAggregator())
 				.print();
 		
-		streamStations.filter(new SensorFilter("COUNT_TI"))
+		streamStations.filter(new SensorFilter(new String[]{"COUNT_TI"}))
 				.map(new TrainStationMapper())
 				.keyBy(0)
 				.window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-				.aggregate(new AverageAggregator("COUNT_TI", countMinSketch))
+				.aggregate(new AverageAggregator())
 				.print();
 		
-		streamStations.filter(new SensorFilter("COUNT_TR"))
+		streamStations.filter(new SensorFilter(new String[]{"COUNT_TR"}))
 				.map(new TrainStationMapper())
 				.keyBy(0)
 				.window(TumblingProcessingTimeWindows.of(Time.seconds(5)))
-				.aggregate(new AverageAggregator("COUNT_TR", countMinSketch))
+				.aggregate(new AverageAggregator())
 				.print();
 
-		streamStations.filter(new SensorFilter("COUNT_TR"))
+		streamStations.filter(new SensorFilter(new String[]{"COUNT_PE", "COUNT_TI", "COUNT_TR"}))
 				.map(new TrainStationMapper())
 				.keyBy(new MyKeySelector())	
 				.timeWindow(Time.seconds(5))
-				.aggregate(new AverageAggregator("COUNT_TR"), new MyProcessWindowFunction())
+				.aggregate(new AverageAggregator(), new MyProcessWindowFunction())
 				.print();
 		// @formatter:on
 
@@ -90,16 +93,20 @@ public class MultiSensorMultiStationsReadingMqtt2 {
 	public static class SensorFilter implements FilterFunction<MqttSensor> {
 
 		private static final long serialVersionUID = 7991908941095866364L;
-		private String filter;
+		private String[] filter;
 
-		public SensorFilter(String filter) {
+		public SensorFilter(String[] filter) {
 			this.filter = filter;
 		}
 
 		@Override
 		public boolean filter(MqttSensor value) throws Exception {
-			if (value.getKey().f1.equals(filter)) {
-				return true;
+			if (filter != null && filter.length > 0) {
+				for (int i = 0; i < filter.length; i++) {
+					if (filter[i].equals(value.getKey().f1)) {
+						return true;
+					}
+				}
 			}
 			return false;
 		}
@@ -124,89 +131,82 @@ public class MultiSensorMultiStationsReadingMqtt2 {
 	}
 
 	public static class AverageAggregator implements
-			AggregateFunction<Tuple3<Integer, Tuple5<Integer, String, Integer, String, Integer>, Double>, Tuple3<Double, Long, Integer>, Tuple2<String, Double>> {
+			AggregateFunction<Tuple3<Integer, Tuple5<Integer, String, Integer, String, Integer>, Double>, Tuple4<Double, Long, Integer, String>, Tuple2<String, Double>> {
 
 		private static final long serialVersionUID = 7233937097358437044L;
-		private String functionName;
-		private CountMinSketch countMinSketch;
 
-		public AverageAggregator(String functionName, CountMinSketch countMinSketch) {
-			this.functionName = functionName;
-			this.countMinSketch = countMinSketch;
-		}
-
-		public AverageAggregator(String functionName) {
-			this.functionName = functionName;
+		@Override
+		public Tuple4<Double, Long, Integer, String> createAccumulator() {
+			return new Tuple4<>(0.0, 0L, 0, "");
 		}
 
 		@Override
-		public Tuple3<Double, Long, Integer> createAccumulator() {
-			return new Tuple3<>(0.0, 0L, 0);
-		}
-
-		@Override
-		public Tuple3<Double, Long, Integer> add(
+		public Tuple4<Double, Long, Integer, String> add(
 				Tuple3<Integer, Tuple5<Integer, String, Integer, String, Integer>, Double> value,
-				Tuple3<Double, Long, Integer> accumulator) {
-
-			if (countMinSketch != null) {
-				if (value.f1.f1.equals("COUNT_PE")) {
-					// int count = (int) Math.round(value.f2);
-					countMinSketch.updateSketchAsync("COUNT_PE");
-				} else if (value.f1.f1.equals("COUNT_TI")) {
-					// int count = (int) Math.round(value.f2);
-					countMinSketch.updateSketchAsync("COUNT_TI");
-				} else if (value.f1.f1.equals("COUNT_TR")) {
-					// int count = (int) Math.round(value.f2);
-					countMinSketch.updateSketchAsync("COUNT_TR");
-				}
-			}
-
-			return new Tuple3<>(accumulator.f0 + value.f2, accumulator.f1 + 1L, value.f1.f4);
+				Tuple4<Double, Long, Integer, String> accumulator) {
+			return new Tuple4<>(accumulator.f0 + value.f2, accumulator.f1 + 1L, value.f1.f4, value.f1.f1);
 		}
 
 		@Override
-		public Tuple2<String, Double> getResult(Tuple3<Double, Long, Integer> accumulator) {
-
-			String label = "";
-			int frequency = 0;
-			if (functionName.equals("COUNT_PE")) {
-				label = "PEOPLE average on train station";
-			} else if (functionName.equals("COUNT_TI")) {
-				label = "TICKETS average on train station";
-			} else if (functionName.equals("COUNT_TR")) {
-				label = "TRAIN average on train station";
-			}
-			if (countMinSketch != null) {
-				frequency = countMinSketch.getFrequencyFromSketch(functionName);
-			}
-
-			return new Tuple2<>(label + "[" + accumulator.f2 + "] reads[" + frequency + "]",
+		public Tuple2<String, Double> getResult(Tuple4<Double, Long, Integer, String> accumulator) {
+			return new Tuple2<>(accumulator.f3 + " " + accumulator.f2 + " ",
 					((double) accumulator.f0) / accumulator.f1);
 		}
 
 		@Override
-		public Tuple3<Double, Long, Integer> merge(Tuple3<Double, Long, Integer> a, Tuple3<Double, Long, Integer> b) {
-			return new Tuple3<>(a.f0 + b.f0, a.f1 + b.f1, a.f2);
+		public Tuple4<Double, Long, Integer, String> merge(Tuple4<Double, Long, Integer, String> a,
+				Tuple4<Double, Long, Integer, String> b) {
+			return new Tuple4<>(a.f0 + b.f0, a.f1 + b.f1, a.f2, a.f3);
 		}
 	}
 
-	public static class MyProcessWindowFunction
-			extends ProcessWindowFunction<Tuple2<String, Double>, Tuple2<String, Double>, Integer, TimeWindow> {
+	/**
+	 * This class holds the state of all windows since the beginning of the data
+	 * stream processing.
+	 * 
+	 * <pre>
+	 * The output of the UDF is <ID, GLOBAL, AVG> where:
+	 * 
+	 * ID: sensor Id + station Id
+	 * GLOBAL: amount of average took over global computation
+	 * AVG: average of the sensor Id from the previous AggregationFunction
+	 * </pre>
+	 * 
+	 * @author Felipe Oliveira Gutierrez
+	 */
+	public static class MyProcessWindowFunction extends
+			ProcessWindowFunction<Tuple2<String, Double>, Tuple3<String, Integer, Double>, Integer, TimeWindow> {
 
 		private static final long serialVersionUID = -40874489412082797L;
 
+		private ValueStateDescriptor<CountMinSketch> descriptorGlobal = new ValueStateDescriptor<>(
+				"countMinSketchState", CountMinSketch.class);
+
+		private ValueStateDescriptor<Tuple2<String, Double>> descriptorWindow = new ValueStateDescriptor<>("state",
+				TupleTypeInfo.getBasicTupleTypeInfo(String.class, Double.class));
+
 		@Override
 		public void process(Integer key,
-				ProcessWindowFunction<Tuple2<String, Double>, Tuple2<String, Double>, Integer, TimeWindow>.Context context,
-				Iterable<Tuple2<String, Double>> counts, Collector<Tuple2<String, Double>> out) throws Exception {
+				ProcessWindowFunction<Tuple2<String, Double>, Tuple3<String, Integer, Double>, Integer, TimeWindow>.Context context,
+				Iterable<Tuple2<String, Double>> counts, Collector<Tuple3<String, Integer, Double>> out)
+				throws Exception {
 
-			System.out.println("key: " + key);
-			System.out.println("context: " + context);
-			System.out.println("context globalState: " + context.globalState());
-			System.out.println("context windowState: " + context.windowState());
-			System.out.println("counts: " + counts);
-			System.out.println("out: " + out);
+			// This is my state over all windows since the beginning of the data stream
+			// processing
+			ValueState<CountMinSketch> stateGlobal = context.globalState().getState(descriptorGlobal);
+
+			CountMinSketch currentGlobalValue = stateGlobal.value();
+			if (currentGlobalValue == null) {
+				currentGlobalValue = new CountMinSketch();
+			}
+			Tuple2<String, Double> v = Iterables.getOnlyElement(counts);
+			currentGlobalValue.updateSketchAsync(v.f0);
+
+			// update the global state with the value of the current window
+			stateGlobal.update(currentGlobalValue);
+
+			// update the result of the ProcessWindowFunction
+			out.collect(Tuple3.of(v.f0, currentGlobalValue.getFrequencyFromSketch(v.f0), v.f1));
 		}
 	}
 }
