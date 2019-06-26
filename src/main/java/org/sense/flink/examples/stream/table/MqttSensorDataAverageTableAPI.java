fa5eefae93cf953110082980ad881a535cd39f8e
Felipe Oliveira Gutierrez <felipe.o.gutierrez@gmail.com>
2019-06-26 12:02:50 +0200

    code to use Calcite rule plan on Flink Table API

--- src/main/java/org/sense/flink/examples/stream/table/MqttSensorDataAverageTableAPI.java
+++ src/main/java/org/sense/flink/examples/stream/table/MqttSensorDataAverageTableAPI.java
@@ -1,12 +1,19 @@
 package org.sense.flink.examples.stream.table;
 
+import org.apache.calcite.rel.rules.FilterMergeRule;
+import org.apache.calcite.tools.RuleSet;
+import org.apache.calcite.tools.RuleSets;
 import org.apache.flink.api.common.time.Time;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.table.api.StreamQueryConfig;
 import org.apache.flink.table.api.Table;
+import org.apache.flink.table.api.TableConfig;
 import org.apache.flink.table.api.Tumble;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
+import org.apache.flink.table.calcite.CalciteConfigBuilder;
+import org.apache.flink.table.catalog.ExternalCatalog;
+import org.apache.flink.table.catalog.InMemoryExternalCatalog;
 import org.apache.flink.types.Row;
 import org.sense.flink.mqtt.MqttSensorTableSource;
 import org.slf4j.Logger;
@@ -18,7 +25,7 @@ public class MqttSensorDataAverageTableAPI {
 	private final String topic_station_01_tickets = "topic-station-01-tickets";
 	private final String topic_station_02_people = "topic-station-02-people";
 	private final String topic_station_02_tickets = "topic-station-02-tickets";
-	// private final String externalCatalog01 = "resources/externalCatalog01.dbc";
+	private final String externalCatalog01 = "resources/externalCatalog01.dbc";
 
 	public static void main(String[] args) throws Exception {
 		// new MqttSensorDataAverageTableAPI("192.168.56.20","192.168.56.1");
@@ -29,6 +36,16 @@ public class MqttSensorDataAverageTableAPI {
 		// Start streaming from fake data source sensors
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
+
+		// change the current calcite config plan
+		// CalciteConfigBuilder ccb = new CalciteConfigBuilder();
+		// RuleSet ruleSets = RuleSets.ofList(FilterMergeRule.INSTANCE);
+		// ccb.addLogicalOptRuleSet(ruleSets);
+		// TableConfig tableConfig = new TableConfig();
+		// tableConfig.setCalciteConfig(ccb.build());
+
+		// Create Stream table environment
+		// StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env, tableConfig);
 		StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 
 		// create an external catalog
