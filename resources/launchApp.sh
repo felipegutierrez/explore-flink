a4f481d117986cd64d998bd9d3e7a3a03c74bb3b
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2019-08-28 11:51:26 +0200

    script to launch application in the cluster

--- resources/launchApp.sh
+++ resources/launchApp.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
+#set -x
+#set -v
 
+FLINK_HOME=/nfshome/felipe/flink-1.9.0
+FLINK_CLI=/nfshome/felipe/flink-1.9.0/bin/flink
+FLINK_APP=/nfshome/felipe/explore-flink.jar
 
-FLINK_HOME=/home/flink/flink-1.9.0
-FLINK_CLI=/home/flink/flink-1.9.0/bin/flink
-FLINK_APP=/home/flink/app/explore-flink.jar
+echo
+echo "Launching a Flink Stream application >>"
+echo "$FLINK_CLI run -c org.sense.flink.App $FLINK_APP -app 30 -source 127.0.0.1 -sink 127.0.0.1 -offlineData true -frequencyPull 10 -frequencyWindow 30 -syntheticData true"
+echo
 
-# launching a Flink stream application
-# ./bin/flink run -c org.sense.flink.App ../app/explore-flink.jar 30 127.0.0.1 192.168.56.1
-OUTPUT=$(echo `$FLINK_CLI run -c org.sense.flink.App $FLINK_APP -app 30 -source 127.0.0.1 -sink 127.0.0.1 -offlineData true -frequencyPull 10 -frequencyWindow 30 -syntheticData true 2>&1`)
+echo "Listing all Flink Stream applications"
+echo "$FLINK_CLI list"
+echo
 
-echo "${OUTPUT}"
-
-# echo `$FLINK_CLI list`
-
+echo "Canceling a Flink Stream application"
+echo "$FLINK_CLI cancel APP_ID"
+echo
