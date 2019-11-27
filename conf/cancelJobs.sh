f8053163d35025124c6f079b4482eba593658ce3
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2019-11-27 19:17:33 +0100

    created script to cancel jobs

--- conf/cancelJobs.sh
+++ conf/cancelJobs.sh
@@ -9,14 +9,14 @@ wget "http://127.0.0.1:8081/jobs/" -O jobsID.json
 for k in $(jq -c '.jobs | .[]' jobsID.json); do
 	echo "================================"
 	#echo $k
-	STATUS=$(echo $k | jq -c '.status')
-	ID=$(echo $k | jq -c '.id')
+	STATUS=$(echo $k | jq -c '.status' | sed "s/\"//g")
+	ID=$(echo $k | jq -c '.id' | sed "s/\"//g")
 	echo "ID: $ID STATUS: $STATUS"
-	if [ "$STATUS" = "\"RUNNING\"" ]; then
+	if [ "$STATUS" = "RUNNING" ]; then
 		read -p "Are you sure you want to cancel JobID $i? (y/n)" -n 1 -r
 		if [[ $REPLY =~ ^[Yy]$ ]]; then
 			echo "   ... canceling Job ID $ID"
-			`$FLINK_CLI cancel $ID`
+			echo `$FLINK_CLI cancel $ID`
 		fi
 	fi
 done
