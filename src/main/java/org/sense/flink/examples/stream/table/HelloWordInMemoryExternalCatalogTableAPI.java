6b496235af8f6eacf06e4ce1f2b0b7d6ebbcb289
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-03-04 11:49:45 +0100

    updated Flink 1.10

--- src/main/java/org/sense/flink/examples/stream/table/HelloWordInMemoryExternalCatalogTableAPI.java
+++ src/main/java/org/sense/flink/examples/stream/table/HelloWordInMemoryExternalCatalogTableAPI.java
@@ -1,23 +1,12 @@
 package org.sense.flink.examples.stream.table;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
-import org.apache.flink.api.common.time.Time;
-import org.apache.flink.api.common.typeinfo.BasicTypeInfo;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.table.api.StreamQueryConfig;
 import org.apache.flink.table.api.java.StreamTableEnvironment;
-import org.apache.flink.table.catalog.ExternalCatalogTable;
-import org.apache.flink.table.catalog.ExternalCatalogTableBuilder;
-import org.apache.flink.table.catalog.InMemoryExternalCatalog;
 import org.apache.flink.table.descriptors.ConnectorDescriptor;
-import org.apache.flink.table.descriptors.Schema;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,12 +19,12 @@ public class HelloWordInMemoryExternalCatalogTableAPI {
 	}
 
 	public HelloWordInMemoryExternalCatalogTableAPI() throws Exception {
-		InMemoryExternalCatalog catalog = new InMemoryExternalCatalog(databaseName);
-		assertTrue(catalog.listTables().isEmpty());
-		catalog.createTable("t1", createTableInstance(), false);
-		List<String> tables = catalog.listTables();
-		assertEquals(1, tables.size());
-		assertEquals("t1", tables.get(0));
+		// InMemoryExternalCatalog catalog = new InMemoryExternalCatalog(databaseName);
+		// assertTrue(catalog.listTables().isEmpty());
+		// catalog.createTable("t1", createTableInstance(), false);
+		// List<String> tables = catalog.listTables();
+		// assertEquals(1, tables.size());
+		// assertEquals("t1", tables.get(0));
 
 		// Start streaming from fake data source sensors
 		StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -43,27 +32,27 @@ public class HelloWordInMemoryExternalCatalogTableAPI {
 		// Create Stream table environment
 		StreamTableEnvironment tableEnv = StreamTableEnvironment.create(env);
 		// obtain query configuration from TableEnvironment
-		//StreamQueryConfig qConfig = tableEnv.queryConfig();
+		// StreamQueryConfig qConfig = tableEnv.queryConfig();
 		// set query parameters
-		//qConfig.withIdleStateRetentionTime(Time.minutes(30), Time.hours(2));
-		
+		// qConfig.withIdleStateRetentionTime(Time.minutes(30), Time.hours(2));
+
 		// Register Data Source Stream tables in the table environment
 		// ExternalCatalogTable t = catalog.getTable("t1");
 		// tableEnv.registerTableSource(name, tableSource);
 		// tableEnv.registerTableSink(name, configuredSink);
 	}
 
-	private ExternalCatalogTable createTableInstance() {
-		TestConnectorDesc connDesc = new TestConnectorDesc("test", 1, false);
-		// @formatter:off
-		Schema schemaDesc = new Schema()
-				.field("first", BasicTypeInfo.STRING_TYPE_INFO)
-				.field("second", BasicTypeInfo.INT_TYPE_INFO);
-		return new ExternalCatalogTableBuilder(connDesc)
-				.withSchema(schemaDesc)
-				.asTableSource();
+	// private ExternalCatalogTable createTableInstance() {
+	// TestConnectorDesc connDesc = new TestConnectorDesc("test", 1, false);
+	// @formatter:off
+	// Schema schemaDesc = new Schema()
+	// 		.field("first", BasicTypeInfo.STRING_TYPE_INFO)
+	// 		.field("second", BasicTypeInfo.INT_TYPE_INFO);
+	// return new ExternalCatalogTableBuilder(connDesc)
+	// 		.withSchema(schemaDesc)
+	// 		.asTableSource();
 		// @formatter:on
-	}
+	// }
 
 	public static class TestConnectorDesc extends ConnectorDescriptor {
 		public TestConnectorDesc(String type, int version, boolean formatNeeded) {
