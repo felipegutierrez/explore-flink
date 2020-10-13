e07427ca0a24b378e31edef9c9712d40b13f1383
Felipe Gutierrez <felipe.gutierrez@tu-berlin.de>
2020-10-13 15:28:50 +0200

    :boom:using exactly-once semantics for Kafka producer

--- docker/ops-playground-image/java/explore-flink/src/main/java/org/sense/flink/examples/stream/tpch/TCPHOrderProducer.java
+++ docker/ops-playground-image/java/explore-flink/src/main/java/org/sense/flink/examples/stream/tpch/TCPHOrderProducer.java
@@ -56,11 +56,11 @@ public class TCPHOrderProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         // producer acks
-        // properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
-        // properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
-        // properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, "3");
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "1");
         // leverage idempotent
-        // properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
         // create producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
 
